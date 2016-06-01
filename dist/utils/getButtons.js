d2f4068ed9b392393624cd15e0d08bdbcb179bc4
jhta <jeisonhs93@gmail.com>
2016-06-01 13:37:55 -0500

    change titles of inline to block

--- dist/utils/getButtons.js
+++ dist/utils/getButtons.js
@@ -18,17 +18,17 @@ var options = _immutable2.default.Map({
   'bold': { label: 'B', style: 'BOLD', classname: 'bold', type: 'inline' },
   'italic': { label: 'I', style: 'ITALIC', classname: 'italic', type: 'inline' },
   'underline': { label: 'U', style: 'UNDERLINE', classname: 'underline', type: 'inline' },
-  'h1': { label: 'H1', style: 'h1', classname: 'h1' },
-  'h2': { label: 'H2', style: 'h2', classname: 'h2' },
-  'h3': { label: 'H3', style: 'h3', classname: 'h3' },
-  'h4': { label: 'H4', style: 'h4', classname: 'h4' },
+  'h1': { label: 'H1', style: 'h1', classname: 'h1', type: 'inline' },
+  'h2': { label: 'H2', style: 'h2', classname: 'h2', type: 'inline' },
+  'h3': { label: 'H3', style: 'h3', classname: 'h3', type: 'inline' },
+  'h4': { label: 'H4', style: 'h4', classname: 'h4', type: 'inline' },
   'unordered-list': { label: 'List', style: 'unordered-list-item', classname: 'list', type: 'block' },
   'ordered-list': { label: 'Ordered List', style: 'ordered-list-item', classname: 'ordered-list', type: 'block' },
   'image': { label: 'Image', style: 'IMAGE', classname: 'image', type: 'image' },
   'link': { label: 'Link', style: 'LINK', classname: 'link', type: 'link' },
   'unlink': { label: 'Unlink', style: 'UNLINK', classname: 'unlink', type: 'unlink' },
   'code': { label: '>_', style: 'code-block', classname: 'code', type: 'block' },
-  'blockquote': { label: 'Blockquote', style: 'blockquote', classname: 'code' },
+  'blockquote': { label: 'Blockquote', style: 'blockquote', classname: 'code', type: 'block' },
   'separator': { type: 'separator' }
 });
 
